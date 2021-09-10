743827bdc0a0018a56021fca3c61c85ba268a5f7
Dillan1903 <A01252454@itesm.mx>
2021-09-10 17:23:25 +0000

    Dillan Meza Practica 1

--- assignments/08PuntoCircunferencia/src/exercise.py
+++ assignments/08PuntoCircunferencia/src/exercise.py
@@ -1,14 +1,13 @@
-
 import math
 def main():
     # Escribe tu código abajo de esta línea
-    r = float(input("Introduce el radio:"))
-    x1 = float(input("Introduce x1: "))
-    y1 = float(input("Introduce y1: "))
-    x2 = float(input("Introduce x2: "))
-    y2 = float(input("Introduce y2: "))
-
-    d=math.sqrt((x2-x1)**2+(y2-y1)**2)
+    r=float(input("Introduce el radio: "))
+    x1=float(input("Introduce x1: "))
+    y1=float(input("Introduce y1: "))
+    x2=float(input("Introduce x2: "))
+    y2=float(input("Introduce y2: "))
+    
+    d=math.sqrt((x2-x1)*2+(y2-y1)*2)
     if d>r:
         print("FUERA")
     elif d<r:
@@ -17,6 +16,5 @@ def main():
         print("SOBRE")
 
    
-
 if __name__ == '__main__':
     main()
